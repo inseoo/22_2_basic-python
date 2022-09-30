df5c03430567d0d2368e04425595e22681701bbb
inseoo <112676989+inseoo@users.noreply.github.com>
2022-09-30 12:31:01 +0900

    Add files via upload

---
+++ 2200287 수업시간.py
@@ -0,0 +1,13 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+t.fd(100)
+
+t.rt(100)
+t.fd(100)
+t.rt(100)
+t.fd(100)
+t.rt(100)
+t.fd(100)
